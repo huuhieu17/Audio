8faf0f828c797f91f6fb8f4a5bde32aa8dab2806
h3kt0r <huuhieu1711@gmail.com>
2020-09-08 13:29:22 +0700

    first commit

---
+++ precache-manifest.7b3e6438a92c75a820d8238ba3b6d023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f535d056b7c1b324a2ac18cea34a593c",
+    "url": "/audio-player-demo/index.html"
+  },
+  {
+    "revision": "0eacd517d904c65a4f93",
+    "url": "/audio-player-demo/static/css/main.f35b6b1f.chunk.css"
+  },
+  {
+    "revision": "801914fd9829ed37416b",
+    "url": "/audio-player-demo/static/js/2.3711fd36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/audio-player-demo/static/js/2.3711fd36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eacd517d904c65a4f93",
+    "url": "/audio-player-demo/static/js/main.033bd7e5.chunk.js"
+  },
+  {
+    "revision": "4b82c0e1907826acebf4",
+    "url": "/audio-player-demo/static/js/runtime-main.8a92c833.js"
+  }
+]);
